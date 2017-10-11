a2acc6d8a490325b673aef4f8de7b0589382395b
queenfiona <fionaityang@gmail.com>
2017-10-12 00:00:16 +0300

    Rango template and dynamic path to it set

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there world! <br> <a href='/rango/about'>about</a>")
+	return render(request,'rango/index.html',{"boldmessage": "I am a bold text from index"})
 
 def about(request):
 	return HttpResponse("Rango says this is the about page <br> <a href='/rango/'>index</a>")
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# code that determines the absolute path of templates
+TEMPLATE_PATH=os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIRS=(
+    TEMPLATE_PATH,
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
