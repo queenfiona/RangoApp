4445f5299940aa6add33d5c792d6f321d2d44bcc
queenfiona <fionaityang@gmail.com>
2017-10-11 22:43:50 +0300

    About pageaccessed through the index page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there world!")
+	return HttpResponse("Rango says hey there world! <br> <a href='/rango/about'>about</a>")
+
+def about(request):
+	return HttpResponse("Rango says this is the about page <br> <a href='/rango/'>index</a>")
