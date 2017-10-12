41ff15106ac3dbc1a44f6dff9372a3fbe8e8738a
queenfiona <fionaityang@gmail.com>
2017-10-12 21:48:28 +0300

    Category and Page models

---
+++ tango_with_django_project/rango/migrations/0002_auto_20171012_1825.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='category',
+            options={'verbose_name_plural': 'Categories'},
+        ),
+        migrations.AlterModelOptions(
+            name='page',
+            options={'verbose_name_plural': 'Pages'},
+        ),
+    ]
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	"""docstring for Category"""
+	name=models.CharField(max_length=128,unique=True)
+
+	def __unicode__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural= "Categories"
+		
+		
+class Page(models.Model):
+	"""docstring for Page"""
+	category=models.ForeignKey(Category, on_delete= models.CASCADE)
+	title=models.CharField(max_length=128)
+	url=models.URLField()
+	views=models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		verbose_name_plural= "Pages"
+		
+
+		
