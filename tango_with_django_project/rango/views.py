21bd83f90df1e1ce9f455798650952ac0ee90bc7
queenfiona <fionaityang@gmail.com>
2017-10-12 19:56:00 +0300

    About page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,4 +5,4 @@ def index(request):
 	return render(request,'rango/index.html',{"boldmessage": "I am a bold text from index"})
 
 def about(request):
-	return HttpResponse("Rango says this is the about page <br> <a href='/rango/'>index</a>")
+	return render(request,'rango/about.html',{"aboutmessage" : "This is the about text from the about view"})
