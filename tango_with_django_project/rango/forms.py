587481f34efd7c2a366a56b9ad3a323461ea1ede
queenfiona <fionaityang@gmail.com>
2017-11-30 13:08:26 +0300

    UserProfileForm and UserForm

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Category,Page
+from django.contrib.auth.models import User
+from rango.models import Category,Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
 	name=forms.CharField(max_length=128,help_text="Please enter the category name.")
@@ -44,4 +45,17 @@ class PageForm(forms.ModelForm):
 		model=Page
 		exclude=('category',)
 		# or specifify fields to include (i.e exclude 'category') e.g fields('title','url','views')
+
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput())
+
+	class Meta:
+		model=User
+		fields = ('username','email','password')
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model =UserProfile
+		fields =('website','picture')
+
 
